7b74e6c56361032561fec5579ca76f11662f1268
User <User@SRV-PO>
2024-11-26 09:13:34 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+
+        static int[] n = { 1, 2, 3, 11, 8, 4 };
+
+        static int m;
+        static void Main(string[] args)
+        {
+            Sortonemassive();
+            Printonemassive();
+        }
+
+
+        static void Sortonemassive()
+        {
+            for (int i = 0; i < n.Length; i++)
+            {
+                for (int j = i + 1; j < n.Length; j++)
+                {
+                    if (n[i] > n[j])
+                    {
+                        m = n[i];
+                        n[i] = n[j];
+                        n[j] = m;
+                    }
+                }
+
+            }
+        }
+
+        static void Printonemassive()
+        {
+            for (int i = 0; i < n.Length; i++)
+            {
+                Console.WriteLine(n[i]);
+            }
+        }
+    }
+}
